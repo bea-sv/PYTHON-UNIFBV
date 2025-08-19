eec68f405b3e19ad801b4e1b642b5fd99fe8d4f7
Beatriz de Souza <bea.souzav0@gmail.com>
2025-08-19 19:10:53 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,21 @@
+#questão 1
+nome=input('Qual é o seu nome?')
+cidade=input('Qual é a sua cidade?')
+
+print('Bem vindo',nome,'como está o dia em',cidade,'?')
+
+#questão 2
+a=int(input('Digite um número:'))
+b=int(input('Digite mais um número:'))
+c=int(input('Digite outro número:'))
+
+soma=a+c
+multi=a*b
+
+print('A soma de a e c  é:',soma)
+print('A multiplicação de b e c é:',multi)
+
+#questao 3
+tempc=int(input('Digite a temperatura em C°:'))
+f=(tempc*1.8)+32
+print(tempc,'C° é igual a',f,'F°')
